59d97dfb7fb9269e757b6517645ab7737a004921
huisheng.jin <huisheng.jinge@gmail.com>
2021-01-13 21:28:50 +0800

    feat: 增加健壮性测试

--- src/test/java/xunit/testsuite/TestSuiteTest.java
+++ src/test/java/xunit/testsuite/TestSuiteTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import xunit.TestResult;
 import xunit.TestUtil;
 import xunit.testcase.SingleMethodTestCase;
+import xunit.testcaseexecute.ExecuteMultipleMethodTestCase;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,4 +24,26 @@ public class TestSuiteTest {
         assertThat(testMethodResult.getMethodName()).isEqualTo(EXECUTE_SUCCESS_TEST);
         assertThat(testMethodResult.getSuccess()).isEqualTo(true);
     }
+
+    @Test
+    void robust_test() {
+        TestSuite testSuite = new TestSuite(Arrays.asList(
+                new SingleMethodTestCase(),
+                new ExecuteMultipleMethodTestCase()
+        ));
+        List<TestClassResult> results = testSuite.execute();
+        assertThat(results.size()).isEqualTo(2);
+        assertThat(results.get(0).getTestResults().size()).isEqualTo(1);
+
+        TestClassResult secondTestClassResult = results.get(1);
+        assertThat(secondTestClassResult.getTestResults().size()).isEqualTo(2);
+        assertThat(secondTestClassResult.getClassName())
+                .isEqualTo(TestUtil.getClassName(ExecuteMultipleMethodTestCase.class));
+
+        TestResult firstTestMethodResult = secondTestClassResult.getTestResults().get(0);
+        assertThat(firstTestMethodResult.getMethodName()).isEqualTo(EXECUTE_FAILURE_TEST);
+        assertThat(firstTestMethodResult.getSuccess()).isEqualTo(false);
+        assertThat(firstTestMethodResult.getFailureMessage()).containsSequence(EXPECTING_FAILURE_MSG_PREFIX);
+    }
+
 }
