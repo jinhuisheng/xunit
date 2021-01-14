8d6a8b9a834a158e45a5dd820811218bd7967407
huisheng.jin <huisheng.jinge@gmail.com>
2021-01-14 22:10:05 +0800

    refactor: 将setup、teardown的测试方法的执行由junit改为testCaseSuit

--- src/test/java/xunit/setupteardown/SetupTeardownTestCase.java
+++ src/test/java/xunit/setupteardown/SetupTeardownTestCase.java
@@ -1,15 +1,15 @@
 package xunit.setupteardown;
 
-import org.junit.jupiter.api.Test;
 import xunit.Constants;
+import xunit.testcase.RTWTestCase;
 import xunit.testcase.TestResult;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SetupTeardownTest {
-    @Test
+public class SetupTeardownTestCase extends RTWTestCase {
+
     void use_set_up() {
         HasSetupMethodTestCase testCase = new HasSetupMethodTestCase();
         List<TestResult> testResults = testCase.executeMethods();
@@ -17,7 +17,6 @@ public class SetupTeardownTest {
         assertThat(Constants.USED_IN_SETUP_LIST.size()).isEqualTo(2);
     }
 
-    @Test
     void use_set_up_and_tear_down() {
         HasSetupAndTearDownMethodTestCase testCase = new HasSetupAndTearDownMethodTestCase();
         List<TestResult> testResults = testCase.executeMethods();
@@ -25,5 +24,8 @@ public class SetupTeardownTest {
         assertThat(Constants.USED_IN_SETUP_AND_TEARDOWN_LIST.size()).isEqualTo(0);
     }
 
-
+    public SetupTeardownTestCase() {
+        this.registerMethod("use_set_up", this::use_set_up);
+        this.registerMethod("use_set_up_and_tear_down", this::use_set_up_and_tear_down);
+    }
 }
