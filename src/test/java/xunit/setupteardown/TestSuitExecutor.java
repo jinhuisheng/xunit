8f8f2560147f0f924fe37995ae9508d3c2a41bef
huisheng.jin <huisheng.jinge@gmail.com>
2021-01-17 23:53:28 +0800

    feat: 换成xunit执行测试

---
+++ src/test/java/xunit/setupteardown/TestSuitExecutor.java
@@ -0,0 +1,31 @@
+package xunit.setupteardown;
+
+import xunit.testcase.TestResult;
+import xunit.testsuite.TestClassResult;
+import xunit.testsuite.TestSuite;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestSuitExecutor {
+    public static void main(String[] args) {
+        TestSuite testSuite = new TestSuite(Arrays.asList(new SetupTeardownTestCase()));
+        List<TestClassResult> result = testSuite.execute();
+        result.forEach(TestSuitExecutor::outputTestClassResult);
+    }
+
+    private static void outputTestClassResult(TestClassResult testClassResult) {
+        System.out.println("\n测试执行结果:\n");
+        System.out.println("测试类:" + testClassResult.getClassName() + ":\n");
+        for (TestResult testResult : testClassResult.getTestResults()) {
+            outputTestMethodResult(testResult);
+        }
+    }
+
+    private static void outputTestMethodResult(TestResult testResult) {
+        System.out.println(String.format("method:%s,result:%s,msg:%s",
+                testResult.getMethodName(),
+                testResult.getSuccess(),
+                testResult.getFailureMessage()));
+    }
+}
